ab359a6a8887487e3e36f2988f6c2d2f55598485
Nimitt Sharma <84977996+Nimittxo@users.noreply.github.com>
2024-06-27 09:33:29 +0530

    Create bubble.rs

---
+++ RUST/sorting algorithms/bubble.rs
@@ -0,0 +1,10 @@
+fn bubble_sort(arr: &mut [i32]) {
+    let len = arr.len();
+    for i in 0..len {
+        for j in 0..len - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
